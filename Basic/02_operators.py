b4fa718193ee216bc5520b91cf3c10dc4b764b44
Sofii <micaelaloprete7@gmail.com>
2025-06-04 09:41:09 -0300

    Tp3
    D

--- Basic/02_operators.py
+++ Basic/02_operators.py
@@ -3,25 +3,25 @@
 ### Operadores Aritméticos ###
 
 # Operaciones con enteros
-print(3 + 4)
-print(3 - 4)
-print(3 * 4)
-print(3 / 4)
-print(10 % 3)
-print(10 // 3)
-print(2 ** 3)
-print(2 ** 3 + 3 - 7 / 1 // 4)
+print(2 + 2)
+print(2 - 2)
+print(2* 2)
+print(2 / 2)
+print(10 % 2)
+print(10 // 2)
+print(2 ** 2)
+print(2 ** 2 + 2 - 3 / 4// 4)
 
 # Operaciones con cadenas de texto
-print("Hola " + "Python " + "¿Qué tal?")
-print("Hola " + str(5))
+print("Buenas " + "Python " + "¿Cómo estas?")
+print("Buenas" + str(2))
 
 # Operaciones mixtas
-print("Hola " * 5)
-print("Hola " * (2 ** 3))
+print("Buenas " * 2)
+print("Buenas" * (2 ** 2))
 
 my_float = 2.5 * 2
-print("Hola " * int(my_float))
+print("Buenas " * int(my_float))
 
 ### Operadores Comparativos ###
 
@@ -34,20 +34,20 @@ print(3 == 4)
 print(3 != 4)
 
 # Operaciones con cadenas de texto
-print("Hola" > "Python")
-print("Hola" < "Python")
+print("Buenas" > "Python")
+print("Buenas" < "Python")
 print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
 print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
-print("Hola" <= "Python")
-print("Hola" == "Hola")
-print("Hola" != "Python")
+print("Buenas" <= "Python")
+print("Buenas" == "Buenas")
+print("Buenas" != "Python")
 
 ### Operadores Lógicos ###
 
 # Basada en el Álgebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole
-print(3 > 4 and "Hola" > "Python")
-print(3 > 4 or "Hola" > "Python")
-print(3 < 4 and "Hola" < "Python")
-print(3 < 4 or "Hola" > "Python")
-print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(3 > 4 and "Buenas" > "Python")
+print(3 > 4 or "Buenas" > "Python")
+print(3 < 4 and "Buenas" < "Python")
+print(3 < 4 or "Buenas" > "Python")
+print(3 < 4 or ("Buenas" > "Python" and 4 == 4))
 print(not (3 > 4))
